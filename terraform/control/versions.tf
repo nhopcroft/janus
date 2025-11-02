487e63ec3703194e70e2a87782480f5a32d2794c
Neil Hopcroft <developer@neilhopcroft.com>
2025-11-02 08:36:23 +0000

    Update spacelift provider version

--- terraform/control/versions.tf
+++ terraform/control/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     spacelift = {
       source  = "spacelift-io/spacelift"
-      version = "1.14.0"
+      version = "1.35.1"
     }
     aws = {
       source  = "hashicorp/aws"
