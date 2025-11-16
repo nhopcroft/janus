68450eadb92046c2e3455eeb2adaed61bf50f683
Neil Hopcroft <developer@neilhopcroft.com>
2025-11-16 21:49:00 +0000

    Subnet ids

--- terraform/stacks/load-balancer/load-balancer.tf
+++ terraform/stacks/load-balancer/load-balancer.tf
@@ -1,5 +1,5 @@
 
 resource "aws_lb" "load_balancer" {
     name = "nh-load-balancer"
-    subnets = [ aws_subnet.public_a, aws_subnet.public_b]
+    subnets = [ aws_subnet.public_a.id, aws_subnet.public_b.id]
 }
