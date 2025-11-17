ad75973bf9a48d9f8b1613f9f818d6d4dbded771
Neil Hopcroft <developer@neilhopcroft.com>
2025-11-17 21:16:24 +0000

    Add certificate

--- terraform/stacks/load-balancer/load-balancer.tf
+++ terraform/stacks/load-balancer/load-balancer.tf
@@ -17,3 +17,38 @@ resource "aws_lb_listener" "http" {
         }
     }
 }
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "prz378.com"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "aws_route53_zone" "prz378" {
+    name = "prz378.com"
+}
+
+resource "aws_route53_record" "domain_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.prz378.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.prz378.zone_id
+}
+
+resource "aws_acm_certificate_validation" "certificate_validation" {
+  certificate_arn         = aws_acm_certificate.prz378.arn
+  validation_record_fqdns = [for record in aws_route53_record.prz378 : record.fqdn]
+}
