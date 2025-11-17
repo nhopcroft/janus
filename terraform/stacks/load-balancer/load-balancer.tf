1ebbb324422886c443ceb249c56e5e1c67ba2281
Neil Hopcroft <developer@neilhopcroft.com>
2025-11-17 21:31:37 +0000

    https

--- terraform/stacks/load-balancer/load-balancer.tf
+++ terraform/stacks/load-balancer/load-balancer.tf
@@ -18,7 +18,7 @@ resource "aws_lb_listener" "http" {
     }
 }
 
-resource "aws_lb_listener" "http" {
+resource "aws_lb_listener" "https" {
     load_balancer_arn = aws_lb.load_balancer.arn
     port = "443"
     protocol = "HTTPS"
