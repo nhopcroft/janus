0457f0688b373f505c90c8fe7c7a3a1793c9eaf5
Neil Hopcroft <developer@neilhopcroft.com>
2025-11-17 07:35:07 +0000

    Import listener

--- terraform/stacks/load-balancer/load-balancer.tf
+++ terraform/stacks/load-balancer/load-balancer.tf
@@ -3,3 +3,17 @@ resource "aws_lb" "load_balancer" {
     name = "nh-load-balancer"
     subnets = [ aws_subnet.public_a.id, aws_subnet.public_b.id]
 }
+
+resource "aws_lb_listener" "https" {
+    load_balancer_arm = aws_lb.load_balancer.arn
+    port = "80"
+    protocol = "HTTP"
+    default_action {
+        type = "fixed-response"
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "Hello"
+            status_code = "200"
+        }
+    }
+}
