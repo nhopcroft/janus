9bff5c9678e8950c2f6d4cdf9da1f24609872459
Neil Hopcroft <dev@neilhopcroft.com>
2026-07-04 20:16:23 +0100

    Add taking.ninja DNS zone

--- terraform/stacks/dns/taking.ninja.tf
+++ terraform/stacks/dns/taking.ninja.tf
@@ -7,6 +7,6 @@ resource "aws_route53_record" "taking_ns" {
     zone_id = aws_route53_zone.taking_ninja
     name = "taking.ninja"
     type = "NS"
-    ttl = "30
+    ttl = "30"
     records = aws_route53_zone.name_servers
 }
