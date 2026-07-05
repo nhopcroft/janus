11bff72aa920146c645471d09b89d974024362b2
Neil Hopcroft <dev@neilhopcroft.com>
2026-07-05 10:58:10 +0100

    Output zone ids

---
+++ terraform/stacks/dns/output.tf
@@ -0,0 +1,8 @@
+
+output "TF_VAR_taking_ninja_zone_id" {
+    value = aws_route53_zone.taking_ninja.zone_id
+}
+
+output "TF_VAR_notetaking_ninja_zone_id" {
+    value = aws_route53_zone.notetaking_ninja.zone_id
+}
