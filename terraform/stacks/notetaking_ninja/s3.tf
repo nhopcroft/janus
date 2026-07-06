3b0e8901ebf3e0b1d1b2007aff5a81bccd5f74dd
Neil Hopcroft <dev@neilhopcroft.com>
2026-07-06 20:56:07 +0100

    Add s3 bucket for static site

--- terraform/stacks/notetaking_ninja/s3.tf
+++ terraform/stacks/notetaking_ninja/s3.tf
@@ -12,5 +12,6 @@ resource "aws_s3_bucket_website_configuration" "notetaking_ninja" {
 
 resource "aws_s3_object" "index" {
     bucket = aws_s3_bucket.notetaking_ninja.id
+    key = "index.html"
     content = "Hello"
 }
