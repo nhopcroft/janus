49278cebdcee3c657cdb918c56cd8ead35cac179
Neil Hopcroft <dev@neilhopcroft.com>
2026-07-06 20:58:22 +0100

    Get the stack tag right

--- terraform/stacks/notetaking_ninja/variables.tf
+++ terraform/stacks/notetaking_ninja/variables.tf
@@ -12,7 +12,7 @@ locals {
     SourceRepo = "https://github.com/nhopcroft/janus/"
     SourceLink = "https://github.com/nhopcroft/janus/tree/main/terraform/stacks/notetaking_ninja"
     Environment = var.deployment_environment != "" ? var.deployment_environment : "n/a"
-    Stack = "DNS"
+    Stack = "notetaking_ninja"
     Note = "This stack contains static resources for notetaking.ninja"
   }
   suffix = length(var.deployment_environment) > 0 ? "-${var.deployment_environment}" : ""
